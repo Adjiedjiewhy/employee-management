a941549f0f702fc44d1e0b21ccbf0f22cb1e4680
Adjiedjiewhy <Adjie.wahyuw@gmail.com>
2024-01-20 14:56:48 +0700

    update 2

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { EMPLOYEES } from '../data/constants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  employees: any;
 
+  ngOnInit() {
+    this.employees = [...EMPLOYEES];
+    console.log(this.employees[69]);
+  }
 }
